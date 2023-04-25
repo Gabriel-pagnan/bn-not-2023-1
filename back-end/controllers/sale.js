34216b11469391940110410ec43cd6bca3ddd7b2
Gabriel-pagnan <102375991+Gabriel-pagnan@users.noreply.github.com>
2023-04-25 00:43:18 +0000

    feat: delete

--- back-end/controllers/sale.js
+++ back-end/controllers/sale.js
@@ -5,8 +5,8 @@ const controller = {}
 controller.create = async (req, res) => {
     try {
         //manda as informações que vieram em req.body para serem gravadas no banco de dados
-       await Sale.create(req.body);
-        
+        await Sale.create(req.body);
+
         //HTTP 201: created
         res.status(201).end()
     } catch (error) {
@@ -16,7 +16,7 @@ controller.create = async (req, res) => {
     }
 }
 
-controller.retrieveAll = async(req, res) => {
+controller.retrieveAll = async (req, res) => {
     try {
         const result = await Sale.find().populate('customer').populate('items.product')
         res.send(result)
@@ -26,12 +26,12 @@ controller.retrieveAll = async(req, res) => {
     }
 }
 
-controller.retriveOne = async(req, res) => {
+controller.retriveOne = async (req, res) => {
     try {
         const result = await Sale.findById(req.params.id).populate('customer').populate('items.product');
-        if(result){
+        if (result) {
             res.send(result)
-        }else{
+        } else {
             res.status(401).end()
         }
     } catch (error) {
@@ -40,58 +40,72 @@ controller.retriveOne = async(req, res) => {
     }
 }
 
-controller.updateOne = async(req, res) => {
+controller.updateOne = async (req, res) => {
     try {
+
         // Encontra a venda para ser atualizada
-    const sale = await Sale.findById(req.params.id)
-    
-    // items foi passado em req.body
-    if(req.body.items) {
-      // Percorre cada item de req.body, verificando se já existe
-      // ou não em sales.item
-      for(let item of req.body.items) {
-        // Se o item tem _id, é porque já existe ~> É CASO DE ATUALIZAÇÃO
-        if(item._id) {
-                    
-          // Procura cada propriedade no item de req.body e atualiza no documento
-          for(let prop in item) {
-            sale.items.id(item._id)[prop] = item[prop]
-          }
+        const sale = await Sale.findById(req.params.id)
+
+        // items foi passado em req.body
+        if (req.body.items) {
+            // Percorre cada item de req.body, verificando se já existe
+            // ou não em sales.item
+            for (let item of req.body.items) {
+                // Se o item tem _id, é porque já existe ~> É CASO DE ATUALIZAÇÃO
+                if (item._id) {
+
+                    // Verifica se foi passada uma propriedade especial, chamada
+                    // '$_delete', com o valor true e, nesse caso, deleta o subdocumento
+                    if (item['$_delete'] === true) {
+                        sale.items.id(item._id).deleteOne()
+                    }
+                    else {
+                        // Procura cada propriedade no item de req.body e atualiza no documento
+                        for (let prop in item) {
+                            sale.items.id(item._id)[prop] = item[prop]
+                        }
+                    }
+                }
+                // Item não existe ~> É caso de inserção
+                else {
+                    sale.items.push(item)   // Cria um novo item
+                }
+            }
+
+            // Indica que o items foi modificado e deve ser regravado
+            sale.markModified('items')
+
         }
-        // Item não existe ~> É caso de inserção
+
+        // Verifica as demais propriedades do pai (sale) por alterações
+        for (let prop in req.body) {
+            if (prop !== 'items') {  // Items já foi processado acima
+                console.log({ prop })
+                sale[prop] = req.body[prop]
+                sale.markModified(prop)
+            }
+        }
+
+        const result = await sale.save()
+
+        if (result) {
+            // Encontrou e atualizou ~> HTTP 204: No content
+            res.status(204).end()
+        }
         else {
-          sale.items.push(item)   // Cria um novo item
+            // Não encontrou para atualizar ~> HTTP 404: Not found
+            res.status(404).end()
         }
-      }
-
-      // Indica que o items foi modificado e deve ser regravado
-      sale.markModified('items')
-
     }
-
-    // Verifica as demais propriedades do pai (sale) por alterações
-    for(let prop in req.body) {
-      if(prop !== 'items') {  // Items já foi processado acima
-        console.log({prop})
-        sale[prop] = req.body[prop]
-        sale.markModified(prop)
-      }
-    }
-    
-        const result = await sale.save();
-        if(result){
-            res.send(result)
-        }else{
-            res.status(401).end()
-        }
-    } catch (error) {
-        console.error(error);
+    catch (error) {
+        console.error(error)
+        // HTTP 500: Internal Server Error
         res.status(500).send(error)
     }
 }
-controller.delete = async(req, res) => {
+controller.delete = async (req, res) => {
     try {
-        const result = await Sale.findByIdAndDelete({_id: req.params.id});
+        const result = await Sale.findByIdAndDelete({ _id: req.params.id });
         res.send(result)
     } catch (error) {
         console.error(error);
